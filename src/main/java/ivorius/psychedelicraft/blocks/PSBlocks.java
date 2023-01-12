d54c39f5480d48fc904b93e772b7679246d8a0f8
Sollace <sollacea@gmail.com>
2023-01-12 23:25:20 +0000

    Set up correct block sounds

--- src/main/java/ivorius/psychedelicraft/blocks/PSBlocks.java
+++ src/main/java/ivorius/psychedelicraft/blocks/PSBlocks.java
@@ -16,21 +16,22 @@ import net.minecraft.sound.BlockSoundGroup;
 
 public interface PSBlocks {
     Block MASH_TUB = register("mash_tub", new MashTubBlock(Settings.of(Material.WOOD)
+            .sounds(BlockSoundGroup.WOOD)
             .hardness(2).nonOpaque().suffocates(BlockConstructionUtils::never).blockVision(BlockConstructionUtils::never)));
 
-    Block OAK_BARREL = register("oak_barrel", new BarrelBlock(Settings.of(Material.WOOD).hardness(2)));
-    Block SPRUCE_BARREL = register("spruce_barrel", new BarrelBlock(Settings.of(Material.WOOD).hardness(2)));
-    Block BIRCH_BARREL = register("birch_barrel", new BarrelBlock(Settings.of(Material.WOOD).hardness(2)));
-    Block JUNGLE_BARREL = register("jungle_barrel", new BarrelBlock(Settings.of(Material.WOOD).hardness(2)));
-    Block ACACIA_BARREL = register("acacia_barrel", new BarrelBlock(Settings.of(Material.WOOD).hardness(2)));
-    Block DARK_OAK_BARREL = register("dark_oak_barrel", new BarrelBlock(Settings.of(Material.WOOD).hardness(2)));
+    Block OAK_BARREL = register("oak_barrel", new BarrelBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
+    Block SPRUCE_BARREL = register("spruce_barrel", new BarrelBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
+    Block BIRCH_BARREL = register("birch_barrel", new BarrelBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
+    Block JUNGLE_BARREL = register("jungle_barrel", new BarrelBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
+    Block ACACIA_BARREL = register("acacia_barrel", new BarrelBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
+    Block DARK_OAK_BARREL = register("dark_oak_barrel", new BarrelBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
 
-    Block FLASK = register("flask", new FlaskBlock(Settings.of(Material.GLASS).hardness(1)));
-    Block DISTILLERY = register("distillery", new DistilleryBlock(Settings.of(Material.GLASS).hardness(1)));
-    Block BOTTLE_RACK = register("bottle_rack", new BottleRackBlock(Settings.of(Material.WOOD).hardness(0.5F)));
+    Block FLASK = register("flask", new FlaskBlock(Settings.of(Material.GLASS).sounds(BlockSoundGroup.WOOD).hardness(1)));
+    Block DISTILLERY = register("distillery", new DistilleryBlock(Settings.of(Material.GLASS).sounds(BlockSoundGroup.GLASS).hardness(1)));
+    Block BOTTLE_RACK = register("bottle_rack", new BottleRackBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(0.5F)));
 
-    Block DRYING_TABLE = register("drying_table", new DryingTableBlock(Settings.of(Material.WOOD).hardness(2)));
-    Block IRON_DRYING_TABLE = register("iron_drying_table", new DryingTableBlock(Settings.of(Material.METAL).hardness(5)));
+    Block DRYING_TABLE = register("drying_table", new DryingTableBlock(Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(2)));
+    Block IRON_DRYING_TABLE = register("iron_drying_table", new DryingTableBlock(Settings.of(Material.METAL).sounds(BlockSoundGroup.METAL).hardness(5)));
 
     JuniperLeavesBlock JUNIPER_LEAVES = register("juniper_leaves", new JuniperLeavesBlock(BlockConstructionUtils.leaves(BlockSoundGroup.GRASS)));
     JuniperLeavesBlock FRUITING_JUNIPER_LEAVES = register("fruiting_juniper_leaves", new JuniperLeavesBlock(BlockConstructionUtils.leaves(BlockSoundGroup.GRASS)));
@@ -44,12 +45,13 @@ public interface PSBlocks {
     Block COFFEA = register("coffea", new CoffeaPlantBlock(BlockConstructionUtils.plant(BlockSoundGroup.GRASS)));
     Block PEYOTE = register("peyote", new PeyoteBlock(BlockConstructionUtils.plant(BlockSoundGroup.GRASS)));
 
-    Block LATTICE = register("lattice", new LatticeBlock(Block.Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).hardness(0.3F).nonOpaque()));
+    Block LATTICE = register("lattice", new LatticeBlock(Block.Settings.of(Material.WOOD)
+            .sounds(BlockSoundGroup.WOOD).hardness(0.3F).nonOpaque()));
     Block WINE_GRAPE_LATTICE = register("wine_grape_lattice", new WineGrapeLatticeBlock(Block.Settings.of(Material.WOOD)
             .sounds(BlockSoundGroup.WOOD).hardness(0.3F).ticksRandomly().nonOpaque()
     ));
 
-    Block RIFT_JAR = register("rift_jar", new RiftJarBlock(Settings.of(Material.ORGANIC_PRODUCT).hardness(0.5F).sounds(BlockSoundGroup.STONE).nonOpaque()));
+    Block RIFT_JAR = register("rift_jar", new RiftJarBlock(Settings.of(Material.ORGANIC_PRODUCT).hardness(0.5F).sounds(BlockSoundGroup.GLASS).nonOpaque()));
     Block GLITCH = register("glitch", new GlitchedBlock(Settings.of(Material.DECORATION).breakInstantly().hardness(0)
             .emissiveLighting(BlockConstructionUtils::always)
             .air().nonOpaque().noBlockBreakParticles().dropsNothing()
