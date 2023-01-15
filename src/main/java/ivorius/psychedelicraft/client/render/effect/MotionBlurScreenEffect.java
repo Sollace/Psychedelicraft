8051cf323933e736dda066b63fb3d243c3151403
Sollace <sollacea@gmail.com>
2023-01-15 16:49:22 +0000

    Fixed crash when trying to render motion blur effects

--- src/main/java/ivorius/psychedelicraft/client/render/effect/MotionBlurScreenEffect.java
+++ src/main/java/ivorius/psychedelicraft/client/render/effect/MotionBlurScreenEffect.java
@@ -124,7 +124,6 @@ public class MotionBlurScreenEffect implements ScreenEffect {
         @Override
         public void close() {
             textures.forEach(GlTexture::close);
-            textures.clear();
         }
     }
 
