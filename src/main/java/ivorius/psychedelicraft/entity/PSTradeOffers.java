904e2f9d0644dc0391b2b25b52755e2657535531
Sollace <sollacea@gmail.com>
2023-01-15 20:26:43 +0000

    Fix and update drug dealer's trades

--- src/main/java/ivorius/psychedelicraft/entity/PSTradeOffers.java
+++ src/main/java/ivorius/psychedelicraft/entity/PSTradeOffers.java
@@ -36,25 +36,39 @@ public interface PSTradeOffers {
     VillagerProfession DRUG_DEALER_PROFESSION = register("drug_dealer", type -> type.matchesKey(DRUG_DEALER_POI), SoundEvents.ENTITY_WANDERING_TRADER_DRINK_POTION);
 
     static void bootstrap() {
-        TradeOfferHelper.registerVillagerOffers(DRUG_DEALER_PROFESSION, 0, factories -> {
+        TradeOfferHelper.registerVillagerOffers(DRUG_DEALER_PROFESSION, 1, factories -> {
             factories.add(buy(Items.EMERALD, 1, PSItems.CANNABIS_LEAF, 1, 9, 1, 0.7f));
             factories.add(buy(Items.EMERALD, 1, PSItems.CANNABIS_SEEDS, 5, 12, 2, 0.5f));
-            factories.add(buy(Items.EMERALD, 1, PSItems.DRIED_CANNABIS_BUDS, 2, 8, 2, 0.9f));
-            factories.add(buy(Items.EMERALD, 1, PSItems.DRIED_CANNABIS_LEAF, 2, 5, 3, 0.8f));
             factories.add(buy(Items.EMERALD, 1, PSItems.HASH_MUFFIN, 2, 3, 1, 0.7f));
-
-            factories.add(buy(Items.EMERALD, 2, PSItems.BROWN_MAGIC_MUSHROOMS, 8, 3, 3, 0.5f));
-            factories.add(buy(Items.EMERALD, 2, PSItems.RED_MAGIC_MUSHROOMS, 8, 3, 3, 0.5f));
-
             factories.add(buy(Items.EMERALD, 1, PSItems.COCA_LEAVES, 4, 4, 1, 0.5f));
             factories.add(buy(Items.EMERALD, 2, PSItems.COCA_SEEDS, 4, 4, 1, 0.5f));
-            factories.add(buy(Items.EMERALD, 2, PSItems.DRIED_COCA_LEAVES, 20, 3, 2, 0.5f));
-
-            factories.add(buy(Items.EMERALD, 2, PSItems.SYRINGE, 4, 3, 1, 0.5f));
-
-            factories.add(buy(Items.EMERALD, 3, PSItems.DRIED_PEYOTE, 10, 2, 2, 0.5f));
-            factories.add(buy(Items.EMERALD, 1, PSItems.PEYOTE_JOINT, 3, 2, 3, 0.5f));
             factories.add(buy(Items.EMERALD, 1, PSItems.PEYOTE, 5, 4, 4, 0.5f));
+
+            factories.add(buy(Items.EMERALD, 1, PSItems.CIGARETTE, 4, 2, 3, 0.8f));
+        });
+        TradeOfferHelper.registerVillagerOffers(DRUG_DEALER_PROFESSION, 2, factories -> {
+            factories.add(buy(Items.EMERALD, 2, PSItems.DRIED_CANNABIS_BUDS, 2, 8, 2, 0.9f));
+            factories.add(buy(Items.EMERALD, 2, PSItems.DRIED_CANNABIS_LEAF, 2, 5, 3, 0.8f));
+            factories.add(buy(Items.EMERALD, 3, PSItems.DRIED_PEYOTE, 10, 2, 2, 0.5f));
+            factories.add(buy(Items.EMERALD, 3, PSItems.DRIED_COCA_LEAVES, 20, 3, 2, 0.5f));
+
+            factories.add(buy(Items.EMERALD, 1, PSItems.CIGAR, 5, 2, 3, 0.5f));
+            factories.add(buy(Items.EMERALD, 1, PSItems.SMOKING_PIPE, 5, 2, 3, 0.5f));
+            factories.add(buy(Items.EMERALD, 6, PSItems.DRYING_TABLE, 1, 2, 3, 0.5f));
+        });
+        TradeOfferHelper.registerVillagerOffers(DRUG_DEALER_PROFESSION, 3, factories -> {
+            factories.add(buy(Items.EMERALD, 5, PSItems.BROWN_MAGIC_MUSHROOMS, 8, 3, 3, 0.5f));
+            factories.add(buy(Items.EMERALD, 2, PSItems.RED_MAGIC_MUSHROOMS, 8, 3, 3, 0.5f));
+
+            factories.add(buy(Items.EMERALD, 3, PSItems.SYRINGE, 4, 3, 1, 0.5f));
+            factories.add(buy(Items.EMERALD, 3, PSItems.BONG, 4, 3, 1, 0.5f));
+            factories.add(buy(Items.EMERALD, 1, PSItems.PEYOTE_JOINT, 3, 2, 3, 0.5f));
+
+            factories.add(buy(Items.EMERALD, 1, PSItems.JOINT, 2, 2, 3, 0.5f));
+
+            if (Psychedelicraft.getConfig().balancing.enableHarmonium) {
+                factories.add(new TradeOffers.SellDyedArmorFactory(PSItems.HARMONIUM, 3, 7, 2));
+            }
         });
 
         PointOfInterestTypes.register(Registries.POINT_OF_INTEREST_TYPE, DRUG_DEALER_POI, Stream.concat(
@@ -63,7 +77,7 @@ public interface PSTradeOffers {
         ).collect(Collectors.toUnmodifiableSet()), 1, 1);
 
         if (Psychedelicraft.getConfig().balancing.worldGeneration.farmerDrugDeals) {
-            TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 0, factories -> {
+            TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1, factories -> {
                 factories.add(buy(Items.EMERALD, 2, PSItems.WINE_GRAPES, 3, 8, 1, 0.5F));
                 factories.add(buy(Items.EMERALD, 1, PSItems.HOP_CONES, 1, 4, 1, 0.6F));
                 factories.add(buy(Items.EMERALD, 1, PSItems.HOP_SEEDS, 1, 4, 1, 0.4F));
