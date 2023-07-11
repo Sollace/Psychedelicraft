4a52d8c24306bf10b0f6f51e82c63879cf3acda4
Sollace <sollacea@gmail.com>
2023-07-11 19:08:46 +0100

    Clean up the barrel model and add support for placing them upright

--- src/main/java/ivorius/psychedelicraft/client/render/blocks/BarrelBlockEntityRenderer.java
+++ src/main/java/ivorius/psychedelicraft/client/render/blocks/BarrelBlockEntityRenderer.java
@@ -5,12 +5,12 @@
 
 package ivorius.psychedelicraft.client.render.blocks;
 
+import ivorius.psychedelicraft.block.BarrelBlock;
 import ivorius.psychedelicraft.block.entity.BarrelBlockEntity;
 import ivorius.psychedelicraft.client.render.RenderUtil;
 import ivorius.psychedelicraft.fluid.*;
 import ivorius.psychedelicraft.fluid.container.Resovoir;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
@@ -31,15 +31,11 @@ public class BarrelBlockEntityRenderer implements BlockEntityRenderer<BarrelBloc
     @Override
     public void render(BarrelBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertices, int light, int overlay) {
         matrices.push();
-        matrices.translate(0.5F, 0.5F, 0.5F);
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-entity.getCachedState().get(HorizontalFacingBlock.FACING).asRotation()));
+        matrices.translate(0.5F, 0, 0.5F);
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180 - entity.getCachedState().get(BarrelBlock.FACING).asRotation()));
 
-        matrices.push();
-        matrices.translate(0, 1, 0);
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180));
         model.setRotationAngles(entity);
         model.render(matrices, vertices.getBuffer(model.getLayer(getBarrelTexture(entity))), light, overlay, 1, 1, 1, 1);
-        matrices.pop();
 
         Resovoir tank = entity.getTank(Direction.UP);
 
@@ -48,14 +44,19 @@ public class BarrelBlockEntityRenderer implements BlockEntityRenderer<BarrelBloc
             Identifier symbol = fluid.getSymbol(tank.getStack());
 
             if (MinecraftClient.getInstance().getResourceManager().getResource(symbol).isPresent()) {
+                matrices.translate(0, 0.5, 0);
                 float barrelZ = -0.4376F;
                 float iconSize = 0.5F;
-
                 VertexConsumer buffer = vertices.getBuffer(model.getLayer(symbol));
-                RenderUtil.vertex(buffer, matrices, -iconSize, -iconSize, barrelZ, 1, 1, overlay, light);
-                RenderUtil.vertex(buffer, matrices, -iconSize,  iconSize, barrelZ, 1, 0, overlay, light);
-                RenderUtil.vertex(buffer, matrices,  iconSize,  iconSize, barrelZ, 0, 0, overlay, light);
-                RenderUtil.vertex(buffer, matrices,  iconSize, -iconSize, barrelZ, 0, 1, overlay, light);
+
+                for (int i = 0; i < 2; i++) {
+                    RenderUtil.vertex(buffer, matrices, -iconSize, -iconSize, barrelZ, 1, 1, overlay, light);
+                    RenderUtil.vertex(buffer, matrices, -iconSize,  iconSize, barrelZ, 1, 0, overlay, light);
+                    RenderUtil.vertex(buffer, matrices,  iconSize,  iconSize, barrelZ, 0, 0, overlay, light);
+                    RenderUtil.vertex(buffer, matrices,  iconSize, -iconSize, barrelZ, 0, 1, overlay, light);
+
+                    matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180));
+                }
             }
         }
 
