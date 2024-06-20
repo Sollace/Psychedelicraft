d2df81a8c19a10e970ddbf406b5a8cdcb4867ac5
Sollace <sollacea@gmail.com>
2024-06-20 21:19:11 +0100

    Set a comparison for fluids

--- src/main/java/ivorius/psychedelicraft/compat/emi/Main.java
+++ src/main/java/ivorius/psychedelicraft/compat/emi/Main.java
@@ -1,27 +1,22 @@
 package ivorius.psychedelicraft.compat.emi;
 
-import java.util.Objects;
-
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.stack.Comparison;
+import ivorius.psychedelicraft.fluid.SimpleFluid;
 import ivorius.psychedelicraft.item.PSItems;
-import ivorius.psychedelicraft.item.component.ItemFluids;
 
 public class Main implements EmiPlugin {
-
-    static final Comparison COMPARE_FLUID = Comparison.of(
-            (a, b) -> a.getKey() == b.getKey() && ItemFluids.of(a.getItemStack()).isRoughlyEqual(ItemFluids.of(b.getItemStack())),
-            s -> Objects.hash(s.getKey(), ItemFluids.of(s.getItemStack()).getHash())
-    );
-
     @Override
     public void register(EmiRegistry registry) {
-        setComparison(registry, COMPARE_FLUID,
+        setComparison(registry, Comparison.compareComponents(),
                 PSItems.WOODEN_MUG, PSItems.STONE_CUP, PSItems.GLASS_CHALICE,
                 PSItems.SHOT_GLASS, PSItems.BOTTLE, PSItems.MOLOTOV_COCKTAIL, PSItems.SYRINGE,
                 PSItems.FILLED_GLASS_BOTTLE, PSItems.FILLED_BUCKET, PSItems.FILLED_BOWL
         );
+        SimpleFluid.REGISTRY.forEach(fluid -> {
+            registry.setDefaultComparison(fluid, Comparison.compareComponents());
+        });
     }
 
     static void setComparison(EmiRegistry registry, Comparison comparison, Object...keys) {
