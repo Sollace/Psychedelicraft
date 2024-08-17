4d12a871923f2038050ac592e2f1feb23fd0ea5c
Sollace <sollacea@gmail.com>
2024-08-17 11:51:11 +0100

    Update tla and set up comparisons support

--- src/main/java/ivorius/psychedelicraft/compat/emi/Main.java
+++ src/main/java/ivorius/psychedelicraft/compat/emi/Main.java
@@ -2,26 +2,11 @@ package ivorius.psychedelicraft.compat.emi;
 
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
-import dev.emi.emi.api.stack.Comparison;
-import ivorius.psychedelicraft.fluid.SimpleFluid;
-import ivorius.psychedelicraft.item.PSItems;
 
 public class Main implements EmiPlugin {
     @Override
     public void register(EmiRegistry registry) {
-        setComparison(registry, Comparison.compareComponents(),
-                PSItems.WOODEN_MUG, PSItems.STONE_CUP, PSItems.GLASS_CHALICE,
-                PSItems.SHOT_GLASS, PSItems.BOTTLE, PSItems.MOLOTOV_COCKTAIL, PSItems.SYRINGE,
-                PSItems.FILLED_GLASS_BOTTLE, PSItems.FILLED_BUCKET, PSItems.FILLED_BOWL
-        );
-        SimpleFluid.REGISTRY.forEach(fluid -> {
-            registry.setDefaultComparison(fluid, Comparison.compareComponents());
-        });
+
     }
 
-    static void setComparison(EmiRegistry registry, Comparison comparison, Object...keys) {
-        for (var key : keys) {
-            registry.setDefaultComparison(key, c -> comparison);
-        }
-    }
 }
--- src/main/java/ivorius/psychedelicraft/compat/tia/Main.java
+++ src/main/java/ivorius/psychedelicraft/compat/tia/Main.java
@@ -1,11 +1,25 @@
 package ivorius.psychedelicraft.compat.tia;
 
+import java.util.stream.Stream;
+
 import io.github.mattidragon.tlaapi.api.plugin.PluginContext;
 import io.github.mattidragon.tlaapi.api.plugin.TlaApiPlugin;
+import io.github.mattidragon.tlaapi.api.recipe.TlaStackComparison;
+import ivorius.psychedelicraft.fluid.SimpleFluid;
+import ivorius.psychedelicraft.item.PSItems;
 
 public class Main implements TlaApiPlugin {
     @Override
     public void register(PluginContext context) {
         RecipeCategory.bootstrap(context);
+
+        context.getItemComparisons().register(TlaStackComparison.compareComponents(),
+                PSItems.WOODEN_MUG, PSItems.STONE_CUP, PSItems.GLASS_CHALICE,
+                PSItems.SHOT_GLASS, PSItems.BOTTLE, PSItems.MOLOTOV_COCKTAIL, PSItems.SYRINGE,
+                PSItems.FILLED_GLASS_BOTTLE, PSItems.FILLED_BUCKET, PSItems.FILLED_BOWL
+        );
+        context.getFluidComparisons().register(TlaStackComparison.compareComponents(), SimpleFluid.REGISTRY.stream().flatMap(f -> {
+            return Stream.of(f.getPhysical().getStandingFluid(), f.getPhysical().getFlowingFluid());
+        }));
     }
 }
