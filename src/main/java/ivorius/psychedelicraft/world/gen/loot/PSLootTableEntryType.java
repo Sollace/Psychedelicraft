8d29ff32dcaf7f09e06b14f9e2a45329ab86ea41
Sollace <sollacea@gmail.com>
2024-10-15 15:15:51 +0100

    Fix loot tables

--- src/main/java/ivorius/psychedelicraft/world/gen/loot/PSLootTableEntryType.java
+++ src/main/java/ivorius/psychedelicraft/world/gen/loot/PSLootTableEntryType.java
@@ -9,12 +9,6 @@ import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.util.Identifier;
 
 public interface PSLootTableEntryType {
-    /*LootPoolEntryType LOOT_TABLE_EXTENSION = register("loot_table_extension", LootExtensionEntry.CODEC);
-
-    private static LootPoolEntryType register(String id, MapCodec<? extends LootPoolEntry> codec) {
-        return Registry.register(Registries.LOOT_POOL_ENTRY_TYPE, Psychedelicraft.id(id), new LootPoolEntryType(codec));
-    }*/
-
     static void bootstrap() {
         Map<Identifier, Identifier> extentionTableIds = new HashMap<>();
         LootTableEvents.MODIFY.register((key, supplier, source) -> {
@@ -23,11 +17,9 @@ public interface PSLootTableEntryType {
             final boolean isVillagerChest = id.getPath().contains("village");
             if ((isVillagerChest || Psychedelicraft.getConfig().balancing.worldGeneration.villageChests())
             || (!isVillagerChest || Psychedelicraft.getConfig().balancing.worldGeneration.dungeonChests())) {
-                if ("psychedelicraftmc".equalsIgnoreCase(id.getPath())) {
+                if ("psychedelicraftmc".equalsIgnoreCase(id.getNamespace())) {
                     extentionTableIds.put(Identifier.ofVanilla(id.getPath()), id);
                 }
-                //supplier.pool(LootPool.builder()
-                  //      .with(LootExtensionEntry.builder(RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.of("psychedelicraftmc", id.getPath())))));
             }
         });
         LootTableEvents.ALL_LOADED.register((resourceManager, registry) -> {
