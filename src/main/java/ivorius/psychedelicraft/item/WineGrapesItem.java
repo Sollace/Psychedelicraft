12367f72c90fb824ef56e14e50e854a929cd823d
Sollace <sollacea@gmail.com>
2025-03-15 13:57:12 +0000

    Fix infinite grape glitch

--- src/main/java/ivorius/psychedelicraft/item/WineGrapesItem.java
+++ src/main/java/ivorius/psychedelicraft/item/WineGrapesItem.java
@@ -28,7 +28,7 @@ public class WineGrapesItem extends SpecialFoodItem {
         BlockState state = context.getWorld().getBlockState(pos);
 
         if (state.isOf(PSBlocks.LATTICE)) {
-
+            context.getStack().decrementUnlessCreative(1, context.getPlayer());
             context.getWorld().playSoundFromEntity(null, context.getPlayer(), SoundEvents.BLOCK_AZALEA_LEAVES_HIT, context.getPlayer().getSoundCategory(), 1, 1);
             context.getWorld().setBlockState(pos, LatticeBlock.copyStateProperties(PSBlocks.WINE_GRAPE_LATTICE.getDefaultState(), state));
             return ActionResult.SUCCESS;
