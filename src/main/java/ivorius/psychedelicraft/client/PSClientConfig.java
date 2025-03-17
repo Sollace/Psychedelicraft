976aa9d07e6488b006406908918591ede031d35c
Sollace <sollacea@gmail.com>
2025-03-17 23:00:27 +0000

    Add comments to the config file

--- src/main/java/ivorius/psychedelicraft/client/PSClientConfig.java
+++ src/main/java/ivorius/psychedelicraft/client/PSClientConfig.java
@@ -15,31 +15,56 @@ import com.minelittlepony.common.util.settings.Setting;
 import ivorius.psychedelicraft.entity.drug.DrugType;
 
 public class PSClientConfig extends Config {
-    public final Setting<Float> dofFocalPointNear = value("visual", "dofFocalPointNear", 0.2F);
-    public final Setting<Float> dofFocalBlurNear = value("visual", "dofFocalBlurNear", 0F);
-    public final Setting<Float> dofFocalPointFar = value("visual", "dofFocalPointFar", 128F);
-    public final Setting<Float> dofFocalBlurFar = value("visual", "dofFocalBlurFar", 0F);
+    public final Setting<Float> dofFocalPointNear = value("visual", "dofFocalPointNear", 0.2F)
+            .addComment("Sets the distance from the camera of the near focal point when doing depth of field")
+            .addComment("Default: 0.2");
+    public final Setting<Float> dofFocalBlurNear = value("visual", "dofFocalBlurNear", 0F)
+            .addComment("Sets blurring amount at the near focal point")
+            .addComment("Default: 0");
+    public final Setting<Float> dofFocalPointFar = value("visual", "dofFocalPointFar", 128F)
+            .addComment("Sets the distance from the camera of the far focal point when doing depth of field")
+            .addComment("Default: 128 (up to render distance)");
+    public final Setting<Float> dofFocalBlurFar = value("visual", "dofFocalBlurFar", 0F)
+            .addComment("Sets blurring amount at the far focal point")
+            .addComment("Default: 0");
 
-    public final Setting<Boolean> shader2DEnabled = value("visual", "shader2DEnabled", true);
-    public final Setting<Boolean> shader3DEnabled = value("visual", "shader3DEnabled", true);
+    public final Setting<Boolean> shader2DEnabled = value("visual", "shader2DEnabled", true)
+            .addComment("Enables and disables all 2D (screen) shader effects.")
+            .addComment("e.g. Blurring, double vision, sunflare, etc")
+            .addComment("Default: true");
+    public final Setting<Boolean> shader3DEnabled = value("visual", "shader3DEnabled", true)
+            .addComment("Enables and disables all 3D (geometric) shader effects.")
+            .addComment("e.g. Waves")
+            .addComment("Default: true");
 
-    public final Setting<Boolean> doHeatDistortion = value("visual", "doHeatDistortion", true);
-    public final Setting<Boolean> doWaterDistortion = value("visual", "doWaterDistortion", true);
-    public final Setting<Boolean> doMotionBlur = value("visual", "doMotionBlur", true);
+    public final Setting<Boolean> doHeatDistortion = value("visual", "doHeatDistortion", true)
+            .addComment("Sets whether to apply a distortion effect when the player is in arid environments")
+            .addComment("Default: true");
+    public final Setting<Boolean> doWaterDistortion = value("visual", "doWaterDistortion", true)
+            .addComment("Sets whether to apply a distortion effect when the player is submerged")
+            .addComment("Default: true");
+    public final Setting<Boolean> doMotionBlur = value("visual", "doMotionBlur", true)
+            .addComment("Sets whether drug effects are allowed can cause motion blur effects")
+            .addComment("Default: true");
 
-    public final Setting<Float> sunFlareIntensity = value("visual", "sunFlareIntensity", 0.25F);
-    //public final Setting<Integer> shadowPixelsPerChunk = value("visual", "shadowPixelsPerChunk", 256);
+    public final Setting<Float> sunFlareIntensity = value("visual", "sunFlareIntensity", 0.25F)
+            .addComment("Controls the intensity of the sunglare. Set to 0 to disable the sun flare entirely")
+            .addComment("Default: 0.25");
+    public final Setting<Boolean> waterOverlayEnabled = value("visual", "waterOverlayEnabled", true)
+            .addComment("Sets whether a water splash effect should appear when the player gets wet or is outside in the rain")
+            .addComment("Default: true");
+    public final Setting<Boolean> hurtOverlayEnabled = value("visual", "hurtOverlayEnabled", true)
+            .addComment("Sets whether the edges of the screen flash red whenever the player is hurt")
+            .addComment("Default: true");
 
-    public final Setting<Boolean> waterOverlayEnabled = value("visual", "waterOverlayEnabled", true);
-    public final Setting<Boolean> hurtOverlayEnabled = value("visual", "hurtOverlayEnabled", true);
-
-    public final Setting<Vector2f> digitalEffectPixelRescale = value("visual", "digitalEffectPixelRescale", new Vector2f(0.05F, 0.05F));
+    public final Setting<Vector2f> digitalEffectPixelRescale = value("visual", "digitalEffectPixelRescale", new Vector2f(0.05F, 0.05F))
+            .addComment("Scaling ratio for the zero drug's effect")
+            .addComment("Default: {x: 0.05, y: 0.05}");
     private transient float[] digitalEffectPixelRescaleF;
 
-    public final Setting<Set<DrugType<?>>> drugsWithBackgroundMusic = value("audio", "drugsWithBackgroundMusic", DrugType.REGISTRY.stream().collect(Collectors.toUnmodifiableSet()));
-
-    // (Sollace) made transient because this config was disabled before
-    //public transient boolean doShadows = false;
+    public final Setting<Set<DrugType<?>>> drugsWithBackgroundMusic = value("audio", "drugsWithBackgroundMusic", DrugType.REGISTRY.stream().collect(Collectors.toUnmodifiableSet()))
+            .addComment("List of drug ids that should play a background music when the player has that drug active")
+            .addComment("Default: []");
 
     public PSClientConfig(Path path) {
         super(new HeirarchicalJsonConfigAdapter(new GsonBuilder().registerTypeAdapter(DrugType.class, RegistryTypeAdapter.of(DrugType.REGISTRY))), path);
--- src/main/java/ivorius/psychedelicraft/config/PSConfig.java
+++ src/main/java/ivorius/psychedelicraft/config/PSConfig.java
@@ -14,13 +14,28 @@ import net.minecraft.item.ItemGroups;
 public class PSConfig extends Config {
     public static final int MINUTE = 20 * 60;
 
-    public final Setting<Integer> randomTicksUntilRiftSpawn = value("balancing", "randomTicksUntilRiftSpawn", MINUTE * 180);
-    public final Setting<Integer> dryingTableTickDuration = value("balancing", "dryingTableTickDuration", MINUTE * 16);
-    public final Setting<Integer> ironDryingTableTickDuration = value("balancing", "ironDryingTableTickDuration", MINUTE * 12);
-    public final Setting<Integer> slurryHardeningTime = value("balancing", "slurryHardeningTime", MINUTE * 30);
-    public final Setting<Boolean> enableHarmonium = value("balancing", "enableHarmonium", true);
-    public final Setting<Boolean> enableRiftJars = value("balancing", "enableRiftJars", true);
-    public final Setting<Boolean> disableMolotovs = value("balancing", "disableMolotovs", false);
+    public final Setting<Integer> randomTicksUntilRiftSpawn = value("balancing", "randomTicksUntilRiftSpawn", MINUTE * 180)
+            .addComment("Controls how frequently zero rifts spawn.")
+            .addComment("Set to 0 to disable rift spawning entirely")
+            .addComment("Default: " + (MINUTE * 180));
+    public final Setting<Integer> dryingTableTickDuration = value("balancing", "dryingTableTickDuration", MINUTE * 16)
+            .addComment("Sets the number of ticks the wooden drying table takes on average to cook items")
+            .addComment("Default: " + (MINUTE * 16));
+    public final Setting<Integer> ironDryingTableTickDuration = value("balancing", "ironDryingTableTickDuration", MINUTE * 12)
+            .addComment("Sets the number of ticks the stone drying table takes on average to cook items")
+            .addComment("Default: " + (MINUTE * 12));
+    public final Setting<Integer> slurryHardeningTime = value("balancing", "slurryHardeningTime", MINUTE * 30)
+            .addComment("Sets the number of ticks it takes for slurry to congeal into dirt")
+            .addComment("Default: " + (MINUTE * 30));
+    public final Setting<Boolean> enableHarmonium = value("balancing", "enableHarmonium", true)
+            .addComment("Sets whether harmonium is obtainable")
+            .addComment("Default: true");
+    public final Setting<Boolean> enableRiftJars = value("balancing", "enableRiftJars", true)
+            .addComment("Sets whether rift jars are obtainable. Rift jars are only useful if rift spawning is enabled as well.")
+            .addComment("Default: true");
+    public final Setting<Boolean> disableMolotovs = value("balancing", "disableMolotovs", false)
+            .addComment("Sets whether molotov cocktails are (not) obtainable.")
+            .addComment("Default: false");
 
     public final Setting<Generation> worldGeneration = value("balancing", "worldGeneration", new Generation(
             FeatureCustomConfig.DEFAULT, FeatureCustomConfig.DEFAULT,
@@ -30,9 +45,17 @@ public class PSConfig extends Config {
             FeatureCustomConfig.DEFAULT, FeatureCustomConfig.DEFAULT,
             FeatureCustomConfig.DEFAULT,
             true, true, true
-    ));
-    public final Setting<TickRates> fluidAttributes = value("balancing", "fluidAttributes", new TickRates(TickRate.getDefaults()));
-    public final Setting<MessageDistortion> messageDistortion = value("balancing", "messageDistortion", MessageDistortion.BOTH);
+    )).addComment("Settings affecting world generation and in-game mechanics");
+    public final Setting<TickRates> fluidAttributes = value("balancing", "fluidAttributes", new TickRates(TickRate.getDefaults()))
+            .addComment("Sets the rate at which each fluid is processed.")
+            .addComment("Delete the entry for a fluid and restart the game to have the default value for that fluid populated.");
+    public final Setting<MessageDistortion> messageDistortion = value("balancing", "messageDistortion", MessageDistortion.BOTH)
+            .addComment("Sets whether drug effects are able to mess with chat messages.")
+            .addComment("Default: BOTH")
+            .addComment("OUTGOING - Messages others see from you are affected")
+            .addComment("INCOMING - Messages you see from others are affected")
+            .addComment("BOTH - All messages are affected")
+            .addComment("NONE - No messages are affected");
 
     public PSConfig(Path path) {
         super(new HeirarchicalJsonConfigAdapter(new GsonBuilder()
