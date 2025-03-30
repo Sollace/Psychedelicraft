7d7f0a7bfb30159bd20a6c7d57b386cff8979122
Sollace <sollacea@gmail.com>
2025-03-30 02:32:26 +0100

    Fixed unable to bucket place fluids

--- src/main/java/ivorius/psychedelicraft/item/FilledBucketItem.java
+++ src/main/java/ivorius/psychedelicraft/item/FilledBucketItem.java
@@ -208,6 +208,11 @@ public class FilledBucketItem extends Item {
                playEmptyingSound(fluidState.getFluid(), player, world, pos);
                return true;
             }
+        } else if (world.isAir(pos) || placeInto) {
+            if (world.setBlockState(pos, placedBlockState, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD) || state.getFluidState().isStill()) {
+                playEmptyingSound(fluidState.getFluid(), player, world, pos);
+                return true;
+            }
         }
         return false;
     }
