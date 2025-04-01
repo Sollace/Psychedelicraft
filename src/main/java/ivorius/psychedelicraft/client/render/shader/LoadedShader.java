52bfe290d45a2aba76d5ecf5337b307fa6b9475c
Sollace <sollacea@gmail.com>
2025-04-01 23:43:30 +0100

    Fix blur and blur noise effects

--- src/main/java/ivorius/psychedelicraft/client/render/shader/LoadedShader.java
+++ src/main/java/ivorius/psychedelicraft/client/render/shader/LoadedShader.java
@@ -41,7 +41,7 @@ class LoadedShader {
                 processor.render(client.getFramebuffer(), pool, null);
             }
         } catch (Throwable t) {
-            Psychedelicraft.LOGGER.error(t.getMessage());
+            Psychedelicraft.LOGGER.error("Exception applying shader pass: {}", t);
         }
     }
 
