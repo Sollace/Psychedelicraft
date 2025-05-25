a2970ec180159fb1b81c938ecd94809a5182f523
Sollace <sollacea@gmail.com>
2025-05-25 18:20:10 +0100

    Change mixing recipes to fill the target receptical rather than giving a fixed amount. Fixes #96

--- src/main/java/ivorius/psychedelicraft/datagen/providers/recipe/PSRecipeProvider.java
+++ src/main/java/ivorius/psychedelicraft/datagen/providers/recipe/PSRecipeProvider.java
@@ -439,7 +439,7 @@ public class PSRecipeProvider extends FabricRecipeProvider {
             .input(PSItems.OBSIDIAN_DUST).criterion(hasItem(PSItems.OBSIDIAN_DUST), conditionsFromItem(PSItems.OBSIDIAN_DUST))
             .receptical(PSTags.Items.DRUG_RECEPTICALS)
             .offerTo(exporter);
-        MixingRecipeJsonBuilder.create(RecipeCategory.FOOD, PSFluids.CAFFEINE, 500)
+        MixingRecipeJsonBuilder.create(RecipeCategory.FOOD, PSFluids.CAFFEINE, 1)
             .input(PSItems.COFFEE_BEANS, 2).criterion(hasItem(PSItems.COFFEE_BEANS), conditionsFromItem(PSItems.COFFEE_BEANS))
             .receptical(PSTags.Items.DRUG_RECEPTICALS)
             .offerTo(exporter);
@@ -466,7 +466,7 @@ public class PSRecipeProvider extends FabricRecipeProvider {
     }
 
     private static void offerMixing(RecipeExporter exporter, SimpleFluid output, ItemConvertible input, TagKey<Item> receptical) {
-        MixingRecipeJsonBuilder.create(RecipeCategory.FOOD, output, 500)
+        MixingRecipeJsonBuilder.create(RecipeCategory.FOOD, output, 1)
             .input(input, 2).criterion(hasItem(input), conditionsFromItem(input))
             .receptical(receptical)
             .offerTo(exporter);
