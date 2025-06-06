0bfc89c40afd414cf3c2a0b6188186687a327c44
Sollace <sollacea@gmail.com>
2025-06-06 19:24:18 +0100

    Adjust wandering trader trades

--- src/main/java/ivorius/psychedelicraft/entity/PSTradeOffers.java
+++ src/main/java/ivorius/psychedelicraft/entity/PSTradeOffers.java
@@ -198,15 +198,16 @@ public interface PSTradeOffers {
         });
 
         TradeOfferHelper.registerWanderingTraderOffers(1, factories -> {
-            factories.add(sell(6, PSItems.BELLADONNA_SEEDS, 1, 1, 1, 0.8F));
+            factories.add(sell(6, PSItems.BELLADONNA_SEEDS, 1, 1, 2, 0.8F));
             factories.add(sell(5, PSItems.COCA_SEEDS, 1, 2, 1, 0.8F));
             factories.add(sell(7, PSItems.HOP_SEEDS, 1, 2, 1, 0.8F));
-            factories.add(sell(9, PSItems.CANNABIS_SEEDS, 1, 1, 1, 0.8F));
+            factories.add(sell(9, PSItems.CANNABIS_SEEDS, 1, 1, 3, 0.8F));
             factories.add(sell(5, PSItems.COFFEA_CHERRIES, 1, 2, 1, 0.8F));
-            factories.add(sell(7, PSItems.JUNIPER_SAPLING, 1, 1, 1, 0.8F));
-            factories.add(sell(9, PSItems.JIMSONWEED_SEEDS, 1, 1, 1, 0.8F));
+            factories.add(sell(7, PSItems.JUNIPER_SAPLING, 1, 1, 2, 0.8F));
+            factories.add(sell(9, PSItems.JIMSONWEED_SEEDS, 1, 1, 3, 0.8F));
             factories.add(sell(5, PSItems.MORNING_GLORY_SEEDS, 1, 2, 1, 0.8F));
             factories.add(sell(5, PSItems.TOBACCO_SEEDS, 1, 2, 1, 0.8F));
+            factories.add(sell(19, PSItems.BLUE_CRYSTAL_METH, 10, 2, 6, 0.5F));
         });
     }
 
